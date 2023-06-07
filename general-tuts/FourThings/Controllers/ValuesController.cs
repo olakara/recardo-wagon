73fae57baeb6d603112841a71f7fb4e30eed999d
Abdel Raoof <olakara@gmail.com>
2023-06-07 20:26:18 +0400

    feat (four-things): implement working logger using serilog

--- general-tuts/FourThings/Controllers/ValuesController.cs
+++ general-tuts/FourThings/Controllers/ValuesController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FourThings.Logger;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FourThings.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+        
         // GET
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.CustomLog("ValuesController");
             return Ok(new [] {"One","Two"});
         }
     }
---
+++ general-tuts/FourThings/Logger/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace FourThings.Logger
+{
+    public static partial class Log
+    {
+        [LoggerMessage(EventId = 0, Level = LogLevel.Information, Message = "This is logging from: {basic}")]
+        public static partial void CustomLog(this ILogger logger, string basic);
+    }
+}
