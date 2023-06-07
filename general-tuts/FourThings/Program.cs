706712530ba5870035d376a5fde08b4e035c5b23
Abdel Raoof <olakara@gmail.com>
2023-06-07 19:55:08 +0400

    feat (four-things): adding serilog

--- general-tuts/FourThings/Program.cs
+++ general-tuts/FourThings/Program.cs
@@ -1,9 +1,14 @@
+using Serilog;
+
 #pragma warning disable CA1852
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
